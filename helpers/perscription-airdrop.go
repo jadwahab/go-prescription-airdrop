42b5e9291ea1eec0c35492bec95980ac18bc244a
Jad Wahab <15110087+jadwahab@users.noreply.github.com>
2023-10-04 21:41:33 +0300

    test presc working

---
+++ helpers/perscription-airdrop.go
@@ -0,0 +1,114 @@
+package helpers
+
+import (
+	"context"
+	"encoding/hex"
+	"errors"
+	"fmt"
+
+	"github.com/libsv/go-bk/wif"
+	"github.com/libsv/go-bt/v2"
+	"github.com/libsv/go-bt/v2/bscript"
+	"github.com/libsv/go-bt/v2/unlocker"
+	"github.com/mrz1836/go-whatsonchain"
+)
+
+func PrescriptionAirdrop(perscOwnerAddress, inscTxId, inscWIF, fundingWIF string, c whatsonchain.ClientInterface) (string, error) {
+	bsvTx := bt.NewTx()
+
+	inscTxHex, err := c.GetRawTransactionData(context.Background(), inscTxId)
+	if err != nil {
+		return "", err
+	}
+	inscTx, err := bt.NewTxFromString(inscTxHex)
+	if err != nil {
+		return "", err
+	}
+	err = bsvTx.From(inscTxId,
+		0,
+		inscTx.Outputs[0].LockingScriptHexString(),
+		inscTx.Outputs[0].Satoshis)
+	if err != nil {
+		return "", err
+	}
+
+	fundingWif, err := wif.DecodeWIF(fundingWIF)
+	if err != nil {
+		return "", err
+	}
+	// get public key bytes and address
+	fundingPubkey := fundingWif.SerialisePubKey()
+	fundingScript, err := bscript.NewP2PKHFromPubKeyBytes(fundingPubkey)
+	if err != nil {
+		return "", err
+	}
+	fundingAddr, err := bscript.NewAddressFromPublicKeyString(hex.EncodeToString(fundingPubkey), true)
+	if err != nil {
+		return "", err
+	}
+	utxos, err := c.AddressUnspentTransactions(context.Background(), fundingAddr.AddressString)
+	if err != nil {
+		return "", err
+	}
+
+	utxoTxInfo, err := c.GetTxByHash(context.Background(), utxos[0].TxHash)
+	if err != nil {
+		return "", err
+	}
+
+	err = bsvTx.From(utxos[0].TxHash,
+		0,
+		utxoTxInfo.Vout[0].ScriptPubKey.Hex,
+		uint64(utxos[0].Value))
+	if err != nil {
+		return "", err
+	}
+
+	err = bsvTx.PayToAddress(perscOwnerAddress, 1)
+	if err != nil {
+		return "", err
+	}
+
+	if utxos[0].Value < 5 {
+		return "", errors.New("insufficient funds")
+	}
+
+	err = bsvTx.PayTo(fundingScript, uint64(utxos[0].Value-5))
+	if err != nil {
+		return "", err
+	}
+
+	// err = bsvTx.Change(fundingScript, bt.NewFeeQuote())
+	// if err != nil {
+	// 	return "", err
+	// }
+
+	inscWif, err := wif.DecodeWIF(inscWIF)
+	if err != nil {
+		return "", err
+	}
+
+	ug := &unlocker.Getter{PrivateKey: inscWif.PrivKey}
+	u, err := ug.Unlocker(context.Background(), inscTx.Outputs[0].LockingScript)
+	if err != nil {
+		return "", err
+	}
+	err = bsvTx.FillInput(context.Background(), u, bt.UnlockerParams{InputIdx: 0})
+	if err != nil {
+		return "", err
+	}
+
+	ug2 := &unlocker.Getter{PrivateKey: fundingWif.PrivKey}
+	u2, err := ug2.Unlocker(context.Background(), fundingScript) // reuse fundingScript
+	if err != nil {
+		return "", err
+	}
+	err = bsvTx.FillInput(context.Background(), u2, bt.UnlockerParams{InputIdx: 1})
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Println(bsvTx.TxID())
+
+	return bsvTx.String(), nil
+}
---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"time"
+
+	"github.com/jadwahab/go-prescription-airdrop/helpers"
+	"github.com/mrz1836/go-whatsonchain"
+)
+
+func main() {
+	filename := "perscList.json"
+	perscList, err := readFile(filename)
+	if err != nil {
+		fmt.Println("Error reading JSON file:", err)
+		return
+	}
+
+	client := whatsonchain.NewClient(whatsonchain.NetworkMain, nil, nil)
+
+	var perscListSuccess []Prescription
+
+	for _, persc := range perscList {
+		res, err := helpers.PrescriptionAirdrop(
+			persc.OwnerAddress,
+			"e027531de98a7ee6830a45d78e8f2ae873640342fab9190365fb0a27d57ac69a",
+			"L2fgiaz4bpCdvbHvvTgeG4gHC15QFdbwMbwsoyQNTwGVJK4Uido6",
+			"KwPVoki5qyur6JwotJkNXsEEENf46VmtHbMnLESatNoeVhZ1NEGp",
+			client,
+		)
+		fmt.Println(res)
+
+		if err == nil {
+			perscListSuccess = append(perscListSuccess, persc)
+			fmt.Println("Success airdropping:")
+			fmt.Println(persc)
+
+			delay(100)
+		} else {
+			fmt.Println(err)
+		}
+	}
+
+	// Filter out the successful ones from the original array
+	for i, persc := range perscList {
+		for _, successPersc := range perscListSuccess {
+			if persc == successPersc {
+				perscList = append(perscList[:i], perscList[i+1:]...)
+				break
+			}
+		}
+	}
+
+	fmt.Println("Successful List:")
+	fmt.Println(perscListSuccess)
+	fmt.Println("Remaining List:")
+	fmt.Println(perscList)
+
+	// Write perscListSuccess and perscList to separate files
+	if err := writeToFile("perscListSuccess", perscListSuccess); err != nil {
+		fmt.Println("Error writing to success file:", err)
+	}
+
+	if err := writeToFile("perscList", perscList); err != nil {
+		fmt.Println("Error writing to remaining file:", err)
+	}
+}
+
+type Prescription struct {
+	PropsNo      int     `json:"propsNo"`
+	TxId         string  `json:"txid"`
+	Location     string  `json:"location"`
+	Origin       string  `json:"origin"`
+	Seller       *string `json:"seller,omitempty"`
+	OwnerAddress string  `json:"ownerAddress"`
+	Paymail      string  `json:"paymail"`
+}
+
+func readFile(filename string) ([]Prescription, error) {
+	// Read the JSON file
+	fileContent, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	// Unmarshal the JSON data into a slice of Prescription structs
+	var prescriptions []Prescription
+	if err := json.Unmarshal(fileContent, &prescriptions); err != nil {
+		return nil, err
+	}
+
+	return prescriptions, nil
+}
+
+func writeToFile(filename string, data interface{}) error {
+	jsonData, err := json.MarshalIndent(data, "", "    ")
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(filename, jsonData, 0644)
+}
+
+func delay(milliseconds time.Duration) {
+	time.Sleep(milliseconds * time.Millisecond)
+}
